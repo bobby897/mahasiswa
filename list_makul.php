6d7da5bfb62e31a2732803c6692107c30eb8165b
bobby897 <bobbyrizkyanto@gmail.com>
2021-05-01 15:13:24 +0700

    list makul

--- list_makul.php
+++ list_makul.php
@@ -12,40 +12,25 @@ $mahasiswa=query("SELECT * FROM m_makul")
  	<title>List Matakuliah</title>
  </head>
  <body>
- <!-- <?php $i=1; ?>
+  <table border="1" cellpadding="10" cellspacing="0">
+	<tr>
+		<th>ID</th>
+		<th>Nama Mata Kuliah</th>
+		<th>Jumlah SKS</th>
+	</tr>
+<?php $i=1; ?>
  <?php foreach ($mahasiswa as $row){ ?>
- -->
- <!-- <form method="get" action="Latihan.php">
-  -->	
- <?php// echo $_POST['nim']; ?>
-  <table>
-<?php //foreach ($mahasiswa as $row)
-	//if($row['Nim'] == $_POST['nim'])
 
-//{ ?>
-<!-- <li>
-	<input type="hidden" name="id" value="<? echo $row['id']; ?>" >
-
-</li>
- -->
- <?php $i=1; ?>
-<form method="post" action="editmahasiswa.php">
  <tr>
-		<td>ID :</td><td></td><td><input type="text" name="Id" value="<?php echo $row['id']; ?>" ></td>
-		<tr></tr>
-		<td>Nama Matakuliah :</td><td></td><td><input type="text" name="Nama_Makul" value="<?php echo $row['nama_makul']; ?> " ></td>
-		<tr></tr>
-		<td>Jumlah SKS :</td><td></td><td><input type="text" name="Jml_Sks" value="<?php echo $row['jml_sks']; ?>"></td>
-		<tr></tr>
-		</tr>
-				<li>
-				<!-- <button type="submit" name="submit" value="edit">Update</button>
-			 --></li>
-	
-</form> 
-<!-- <?php $i++; } ?>
- -->
- <?php //} ?>
+		
+		<td><?php echo $row['id']; ?></td>
+		<td><?php echo $row['nama_makul']; ?></td>
+		<td><?php echo $row['jml_sks']; ?></td>
+		</td>
+	</tr>
+
+
+ <?php } ?>
  	</table>
  </body>
  </html>
